6d23f02dbfca4f8f94630e6878e632a9cfdc8808
Marie-JG <99450393+Marie-JG@users.noreply.github.com>
2022-08-04 13:48:47 -0300

    Update guia2.js

--- JavaScript/js/guia2.js
+++ JavaScript/js/guia2.js
@@ -1,4 +1,3 @@
-
 //Ejercicio 1 Saber si un número es par
 
 
@@ -79,29 +78,29 @@ if ((genero1 == "M" && edad1 >= 65) || (genero1 == "F" && edad1 >= 60)) {
 
 //Ejercicio 6 Calcular el monto de aporte al sindicato
 
-let edad  =parseInt(prompt("¿Tu edad?"));
-let sueldo = parseInt(prompt("¿Tu sueldo?"));
+let edad  =parseInt(prompt("¿Cuál es tu edad?"));
+let sueldo = parseInt(prompt("¿Cúal es tu sueldo?"));
 let aporte=0;
 if (sueldo<20000 && edad>30){
-    aporte== (sueldo*0.007)
+    aporte= (sueldo*0.007)
 }else{
-    aporte== (sueldo*0.007) + ((sueldo*0.007)*0.3);
+    aporte=((sueldo*0.007) + ((sueldo*0.007)*0.3));
 }
 if (sueldo>20000 && sueldo<29999 && edad>30){
-    aporte== (sueldo*0.014);
+    aporte= (sueldo*0.014);
 }else{
-    aporte== (sueldo*0.014)+ ((sueldo*0.014)*0.3);
+    aporte= ((sueldo*0.014)+ ((sueldo*0.014)*0.3));
 }
 if(sueldo>30000 && sueldo<39999 && edad>30){
-    aporte== (sueldo*0.021);
+    aporte= (sueldo*0.021);
 }else{
-    aporte== (sueldo*0.021)+ ((sueldo*0.021)*0.3);
+    aporte= ((sueldo*0.021)+ ((sueldo*0.021)*0.3));
 }
 
 if (sueldo>40000 && edad>30){
-    aporte== (sueldo*0.028);
+    aporte=(sueldo*0.028);
 }else{
-    aporte== (sueldo*0.028)+ ((sueldo*0.028)*0.3);
+    aporte= ((sueldo*0.028)+ ((sueldo*0.028)*0.3));
 }
 
 
@@ -114,19 +113,24 @@ let num2= parseInt(prompt("Ingrese el segundo número"));
 let num3= parseInt(prompt("Ingrese el tercer número"));
 
 if(num1<num2 && num1<num3){
-    if(num2<num3){
-        console.log(num1 +"-"+num2+"-"+num3)
-    }else{
-        console.log(num1 +"-"+num3+"-"+num2)
-    }
-}else{
-    if(num2<num3 && num2<num1){
-        console.log(num1 +"-"+num2+"-"+num3)
-    }else{
-        console.log(num1)
-    }
+   if(num2<num3){
+    console.log(num1 + ", " + num2 + ", " + num3);
+   }else{
+    console.log(num1 + ", " + num3 + ", " + num2);
+   }
+}else if (num2<num1 && num2< num3){
+  if(num1<num3){
+    console.log(num2 + ", " + num1 + ", " + num3);
+  }else{
+    console.log(num2 + ", " + num3 + ", " + num1);
+  }
+}else if(num3<num1&&num3<num2){
+  if(num1<num2){
+    console.log(num3 + ", " + num1 + ", " + num2);
+  }else{
+    console.log(num3 + ", " + num2 + ", " + num1);
+  }
 }
-
 
 //Ejercicio 8 Realizá un programa que permita al usuario ingresar dos números enteros. La computadora debe
 //indicar si el mayor es divisible por el menor.
@@ -162,7 +166,17 @@ let lado1= parseFloat(prompt("Ingrese lado 1"));
 let lado2= parseFloat(prompt("Ingrese lado 2"));
 let lado3= parseFloat(prompt("Ingrese lado 3"));
 
-if (lado1>)
+if (lado1<0.1 || lado2<0.1 || lado3<0.1){
+  console.log("No es válido")
+}else if ((lado1 != lado2 && lado1 != lado3)&& (lado2 !=lado3)){
+  console.log("El triángulo es escaleno")
+}else if (lado1==lado2 && lado2 == lado3){
+  console.log("El triángulo es equilátero")
+}else if((lado1==lado2 && lado1 !=lado3)|| (lado1==lado3 && lado1!=lado2)||(lado2==lado3&& lado2!=lado1)){
+  console.log("El triángulo es isósceles")
+}else{
+  console.log("El triángulo es válido")
+}
 
 
 
@@ -175,45 +189,45 @@ if (lado1>)
 //computadora debe mostrar por pantalla el nombre del mes del año que representa tal número. Si se
 //ingresa un número fuera de rango, debe mostrar un error.
 
-let numeroMes=parseInt(prompt("Ingrese un numero del 1 al 12 "));
-let texto;
+let numeroMes=parseInt(prompt("Ingrese un el numero del mes (1 al 12)"));
+
 		
 switch (numeroMes) {
     case 1:
-      texto = "Enero";
+      console.log("Enero");
       break;
     case 2:
-      texto = "Febrero";
+      console.log("Febrero");
       break;
     case 3:
-      texto = "Marzo";
+      console.log("Marzo");
       break;
     case 4:
-      texto = "Abril";
+      console.log("Abril");
       break;
     case 5:
-      texto = "Mayo";
+      console.log("Mayo");
       break;
     case 6:
-      texto = "Junio";
+      console.log("Junio");
       break;
     case 7:
-      texto = "Julio";
+      console.log("Julio");
       break;
     case 8:
-      texto = "Agosto";
+      console.log("Agosto");
       break;
     case 9:
-      texto = "Septiembre";
+      console.log("Septiembre");
       break;
     case 10:
-      texto = "Octubre";
+      console.log("Octubre");
       break;
     case 11:
-      texto = "Noviembre";
+      console.log("Noviembre");
       break;
     case 12:
-      texto = "Diciembre";
+      console.log("Diciembre");
       break;
     default:
       texto = "El número ingresado es incorrecto";
@@ -223,33 +237,34 @@ switch (numeroMes) {
 //sistema de numeración romano. La computadora debe mostrar su correspondiente valor decimal.
 //Si se ingresa una letra inexistente, la computadora debe informar que no existe tal dígito.
 
-let digito=prompt("Ingrese un numero romano (I, V, X, L, C, D, M)");   //Esta mal switch admite solo una expresion!!!
+let digito=prompt("Ingrese un numero romano (I, V, X, L, C, D, M)");   
 digito.toUpperCase();
 
 switch (digito) {
     case I:
-      texto = "1";
+      console.log(1);
       break;
     case V:
-      texto = "5";
+      console.log(5);
       break;
     case X:
-      texto = "10";
+      console.log(10);
       break;
     case L:
-      texto = "50";
+      console.log(50);
       break;
     case C:
-      texto = "100";
+      console.log(100);
       break;
     case D:
-      texto = "500";
+      console.log(500);
       break;
     case M:
-      texto = "1000";
+      console.log(1000);
       break;
     default:
-      texto= "El dígito ingresado no corresponde a un número romano"  
+      console.log("El dígito ingresado no corresponde a un número romano");
+        
 }
 
 //Ejercicio 12 Realizá un programa que permita al usuario ingresar el día y el mes de su cumpleaños (de manera
@@ -258,13 +273,12 @@ switch (digito) {
 
 let dia= parseInt(prompt("Ingrese el día de nacimiento"));
 
-
 while(dia<1 || dia>31){
     let dia= parseInt(prompt("Ingrese el día de nacimiento"));
 }
 let mes= parseInt(prompt("Ingrese el mes de nacimiento"));
 
-while (mes<1 || mes>12){
+while(mes<1 || mes>12){
 let mes= parseInt(prompt("Ingrese el mes de nacimiento"));
 }
 
@@ -273,15 +287,15 @@ console.log('Tu signo es Aries');
 }
 
 
-if ((dia>=21&&mes==4)||(dia<=21&&mes==5)){
+else if ((dia>=21&&mes==4)||(dia<=21&&mes==5)){
 console.log('Tu signo es Tauro');
 }
 
-if((dia>=22&&mes==5)||(dia<=21&&mes==6)){
+else if((dia>=22&&mes==5)||(dia<=21&&mes==6)){
 console.log('Tu signo es Géminis');
 }
 
-if ((dia>=21&&mes==6)||(dia<=23&&mes==7)){
+else if ((dia>=21&&mes==6)||(dia<=23&&mes==7)){
 console.log('Tu signo es Cáncer');
 }
 
@@ -289,32 +303,36 @@ if ((dia>=24&&mes==7)||(dia<=23&&mes==8)){
 console.log('Tu sino es Leo');
 }
 
-if ((dia>=24&&mes==8)||(dia<=23&&mes==9)){
+else if ((dia>=24&&mes==8)||(dia<=23&&mes==9)){
 console.log('Tu signo es Virgo');
 }
 
-if ((dia>=24&&mes==9)||(dia<=23&&mes==10)){
+else if ((dia>=24&&mes==9)||(dia<=23&&mes==10)){
 console.log('Tu signo es Libra');
 }
 
-if ((dia>=24&&mes==10)||(dia<=22&&mes==11)){
+else if ((dia>=24&&mes==10)||(dia<=22&&mes==11)){
 console.log('Tu signo es Escorpio');
 }
 
-if ((dia>=23&&mes==11)||(dia<=21&&mes==12)){
+else if ((dia>=23&&mes==11)||(dia<=21&&mes==12)){
 console.log('Tu signo es Sagitario');
 }
 
-if ((dia>=22&&mes==12)||(dia<=20&&mes==1)){
+else if ((dia>=22&&mes==12)||(dia<=20&&mes==1)){
 console.log('Tu signo es Capricornio');
 }
 
-if ((dia>=21&&mes==1)||(dia<=19&&mes==2)){
+else if ((dia>=21&&mes==1)||(dia<=19&&mes==2)){
 console.log('Tu signo es Acuario');
 }
 
-if ((dia>=20&&mes==2)||(dia<=20&&mes==3)){
+else if ((dia>=20&&mes==2)||(dia<=20&&mes==3)){
 console.log('Tu signo es Piscis');
 }
+else {
+    console.log("Error");
+}
+
 
 
