478c2cb4fc127cd3d3e27b2432ad0c1d5eb8b750
Marie-JG <99450393+Marie-JG@users.noreply.github.com>
2022-08-04 14:15:31 -0300

    Update guia2.js

--- JavaScript/js/guia2.js
+++ JavaScript/js/guia2.js
@@ -166,7 +166,7 @@ let lado1= parseFloat(prompt("Ingrese lado 1"));
 let lado2= parseFloat(prompt("Ingrese lado 2"));
 let lado3= parseFloat(prompt("Ingrese lado 3"));
 
-if (lado1<0.1 || lado2<0.1 || lado3<0.1){
+if ((lado1+lado2+lado3) != 180){
   console.log("No es válido")
 }else if ((lado1 != lado2 && lado1 != lado3)&& (lado2 !=lado3)){
   console.log("El triángulo es escaleno")
